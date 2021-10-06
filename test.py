2af00438232de22b64a202d64074a0473b7fc89c
avinashsingh518 <mr.avinash97@gmail.com>
2021-10-06 23:44:41 +0530

    hdh

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 def add_two_num(a, b):
     return a + b
-<<<<<<< HEAD
+
 
 def div(a, b):
     return a/b
 
-=======
+
 print(add_two_num(10,20))
->>>>>>> 07d1280e27b03bd9ad78c75d636e7d9d56a0d265
+
